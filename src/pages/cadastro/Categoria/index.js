30503f9f27f6a19a7b389f59d4d74bc0b0a6768e
Thalita Mayra <45610020+thalitsms@users.noreply.github.com>
2020-09-02 17:50:12 -0300

    add atualizações

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -121,9 +121,9 @@ function CadastroCategoria() {
 
 
       <ul>
-        {categorias.map( ( categoria, indice ) => {
+        {categorias.map( ( categoria ) => {
           return ( 
-            <li key={`${categoria}${indice}`}>
+            <li key={`${categoria.titulo}`}>
               {categoria.titulo}
             </li>
           )
